c77fc0e6c0e0b6a2d31bcf4d0e78aca7e17dc6ce
Chris <litmusik@yahoo.com>
2020-12-06 10:29:14 -0600

    Generating skeletons for blog posts

--- blog_gen.py
+++ blog_gen.py
@@ -2,6 +2,7 @@ import argparse
 import datetime
 import os
 import sys
+import time
 import zipfile
 
 days_in_month = {
@@ -19,6 +20,42 @@ days_in_month = {
     12: 31,
 }
 
+months = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December',
+}
+
+main_link = ('For full context, see the description of my Art Home School Curriculum. '
+             'https://litmusik.medium.com/art-home-school-introduction-f0e7027af017')
+
+def get_next_n_days(n, month, day, year):
+    result = []
+    for _ in range(n):
+        date_dir = "{}_{}_{}".format(month, day, year)
+        result.append(date_dir)
+
+        if day == days_in_month[month]:
+            month += 1
+            day = 1
+        else:
+            day += 1
+
+        if month == 13:
+            month = 1
+            year += 1
+
+    return result
+
 def init_unit(unit_dir, month, day):
     if args.day > days_in_month[args.month]:
         sys.exit("There are only {} days in month {}".format(days_in_month[args.month], args.month))
@@ -27,25 +64,16 @@ def init_unit(unit_dir, month, day):
     assert(args.month)
 
     os.mkdir(unit_dir)
-    year = 20
+    year = int(time.strftime("%y"))
     weeks = ["Week{}".format(x + 1) for x in range(4)]
     for week in weeks:
         week_dir = os.path.join(unit_dir, week)
         os.mkdir(week_dir)
-        for _ in range(7):
-            date_dir = "{}_{}_{}".format(month, day, year)
-            date_path = os.path.join(week_dir, date_dir)
+        days = get_next_n_days(7, month, day, year)
+
+        for date in days:
+            date_path = os.path.join(week_dir, date)
             os.mkdir(date_path)
-
-            if day == days_in_month[month]:
-                month += 1
-                day = 1
-            else:
-                day += 1
-
-            if month == 13:
-                month = 1
-                year += 1
 
 def transfer_photos(unit_dir, week):
     assert(os.path.exists(unit_dir))
@@ -77,25 +105,60 @@ def transfer_photos(unit_dir, week):
             os.remove(zip_path)
 
 
+def gen_plan_skeleton():
+    print(main_link)
+    print()
+    print('Resources')
+    print()
+    print('Art Parent')
+    print()
+    print('Challenges')
+    print()
+    print('Inspiration')
+
+def gen_post_skeleton(month, day):
+    print('For a description of this unit see my {} unit plan. ' + main_link)
+    print()
+    print('Retrospective')
+    print()
+    print('Log')
+    print()
+    print('Drawings and Critiques')
+    print()
+
+    for date in get_next_n_days(7, month, day, int(time.strftime('%y'))):
+        dt = datetime.datetime.strptime(date, '%m_%d_%y')
+        print(dt.strftime('%A, %b. %d'))
+        print()
+
 def main(args):
     base_dir = 'D:/drawing/work'
-    unit_dir = os.path.join(base_dir, args.unit)
 
-    if (args.day and args.month):
-        init_unit(unit_dir, args.month, args.day)
-    elif (args.week):
-        transfer_photos(unit_dir, args.week)
-    else:
-        sys.exit("Either a day or a week is required")
+    if (args.unit):
+        unit_dir = os.path.join(base_dir, args.unit)
+        if (args.day and args.month):
+            init_unit(unit_dir, args.month, args.day)
+        elif (args.week):
+            transfer_photos(unit_dir, args.week)
+        else:
+            sys.exit("Either a day or a week is required with a unit")
+    elif args.plan:
+        gen_plan_skeleton()
+    elif args.post:
+        assert(args.month)
+        assert(args.day)
+        gen_post_skeleton(args.month, args.day)
 
     return 0
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--unit', required=True, help='The name of the unit')
+    parser.add_argument('--unit', help='The name of the unit')
     parser.add_argument('--month', type=int, help='The month number (January = 1)')
     parser.add_argument('--day', type=int, help='The start day of the month')
     parser.add_argument('--week', type=int, help='The week number to unzip photos for')
+    parser.add_argument('--plan', action='store_true', help='Generate a skeleton for a unit plan blog post.')
+    parser.add_argument('--post', action='store_true', help='Generate a skeleton for a weekly post.')
     args = parser.parse_args()
 
     main(args)
